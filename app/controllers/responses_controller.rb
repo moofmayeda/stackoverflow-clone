90a6071b121def09b0fb708fe732c78d96e100be
moof mayeda <moofmayeda@yahoo.com>
2014-09-02 15:22:16 -0700

    add a new response without refreshing the page

--- app/controllers/responses_controller.rb
+++ app/controllers/responses_controller.rb
@@ -1,8 +1,17 @@
 class ResponsesController < ApplicationController
   before_filter :authorize
 
-  def new
+  def create
     @question = Question.find(params[:question_id])
-    @response = current_user.responses.new
+    @response = current_user.responses.create!(response_params.merge(question_id: params[:question_id]))
+    respond_to do |format|
+      format.html { redirect_to user_question_path(@question.user, @question) }
+      format.js
+    end
+  end
+
+private
+  def response_params
+    params.require(:response).permit(:response)
   end
 end
