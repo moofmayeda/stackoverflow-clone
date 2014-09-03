ce3f190956050d903e5c08d1109a812008a1b930
moof mayeda <moofmayeda@yahoo.com>
2014-09-02 17:04:08 -0700

    edit and display updated comments using AJAX

--- app/controllers/responses_controller.rb
+++ app/controllers/responses_controller.rb
@@ -3,7 +3,7 @@ class ResponsesController < ApplicationController
 
   def create
     @question = Question.find(params[:question_id])
-    @response = current_user.responses.create!(response_params.merge(question_id: params[:question_id]))
+    @response = Response.create!(response_params)
     respond_to do |format|
       format.html { redirect_to user_question_path(@question.user, @question) }
       format.js
@@ -15,8 +15,18 @@ class ResponsesController < ApplicationController
     @question = @response.question
   end
 
+  def update
+    @response = Response.find(params[:id])
+    if @response.update(response_params)
+      respond_to do |format|
+        format.html { redirect_to user_question_path(@response.question.user, @response.question) }
+        format.js
+      end
+    end
+  end
+
 private
   def response_params
-    params.require(:response).permit(:response)
+    params.require(:response).permit(:response).merge(user_id: current_user.id, question_id: params[:question_id])
   end
 end
