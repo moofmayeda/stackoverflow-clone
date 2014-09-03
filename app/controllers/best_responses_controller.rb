029781b35da9787bf1ecefd51aa89b6339efbeff
moof mayeda <moofmayeda@yahoo.com>
2014-09-03 15:55:52 -0700

    switch to a different best response using AJAX

--- app/controllers/best_responses_controller.rb
+++ app/controllers/best_responses_controller.rb
@@ -2,13 +2,10 @@ class BestResponsesController < ApplicationController
   def create
     @response = Response.find(params[:response_id])
     @response.question.best_response.destroy
-    best = BestResponse.new(response_id: @response.id, question_id: @response.question.id)
-    if best.save
-      flash[:notice] = "Marked as best answer!"
-      redirect_to user_question_path(@response.question.user, @response.question)
-    else
-      flash[:alert] = "Not marked"
-      redirect_to user_question_path(@response.question.user, @response.question)
+    best = BestResponse.create(response_id: @response.id, question_id: @response.question.id)
+    respond_to do |format|
+      format.html { redirect_to user_question_path(@response.question.user, @response.question) }
+      format.js
     end
   end
 end
