6d2ca1583b4c0a0aad95c2647c07b6f7e0607170
moof mayeda <moofmayeda@yahoo.com>
2014-09-03 17:17:26 -0700

    display the best response first

--- app/controllers/best_responses_controller.rb
+++ app/controllers/best_responses_controller.rb
@@ -1,6 +1,7 @@
 class BestResponsesController < ApplicationController
   def create
     @response = Response.find(params[:response_id])
+    @old_response = @response.question.best_response.response
     @response.question.best_response.destroy
     best = BestResponse.create(response_id: @response.id, question_id: @response.question.id)
     respond_to do |format|
--- app/models/question.rb
+++ app/models/question.rb
@@ -5,4 +5,8 @@ class Question < ActiveRecord::Base
   has_many :responses
   belongs_to :user
   has_one :best_response
+
+  def best_first
+    self.responses.sort_by { |response| BestResponse.find_by(response_id: response.id)? 0 : 1 }
+  end
 end
